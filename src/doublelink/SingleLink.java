bf010049b2c204708bde1850026cd6d3266adc9b
dasongju <lnhjds@126.com>
2018-06-12 06:59:29 +0800

    recusive

--- src/doublelink/SingleLink.java
+++ src/doublelink/SingleLink.java
@@ -53,6 +53,9 @@ public class SingleLink<T> {
         return true;
     }
 
+    /**
+     * 利用pre，cur，ori三个变量，不记得就自己画图，找边界和停止的条件
+     */
     public void reverseTable() {
         Node cur = head.next;
 
@@ -70,6 +73,40 @@ public class SingleLink<T> {
             cur = ori;
         }
     }
+
+    //递归啊，好难啊
+    public Node reverseWithRecusive() {
+        return recusive(head);
+    }
+
+    private Node recusive(Node current) {
+        // 如果当前链表只有一个节点，直接返回即可
+        if (current == null || current.next == null) return current;
+        // 找到当前节点的下个节点
+        Node nextNode = current.next;
+        // 将当前节点的next引用置为null，递归后重新处理
+        current.next = null;
+        Node reverseRest = recusive(nextNode);
+        // 反转链表的实质不就是当前节点被它的后记节点指向
+        nextNode.next = current;
+        return reverseRest;
+    }
+
+    public int length(Node head){
+
+        if(head.next==null){
+            return 0;
+        }
+
+        int i=1;
+        Node temp=head.next;
+        while((temp=temp.next)!=null){
+            i++;
+        }
+
+        return i;
+    }
+
 
     public void print() {
         Node node = head.next;
@@ -92,7 +129,11 @@ public class SingleLink<T> {
         link.add(4);
         link.print();
 
-        link.reverseTable();
-        link.print();
+        SingleLink.Node newNode = link.reverseWithRecusive();
+        System.err.println(newNode.data);
+        System.err.println(newNode.next.data);
+        System.err.println(newNode.next.next.data);
+        System.err.println(newNode.next.next.next.data);
+
     }
 }
