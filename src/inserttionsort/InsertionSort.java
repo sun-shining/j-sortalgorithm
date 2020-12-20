9c2b2f164e85b2050c5ea27c7d0b6fc7eaa62f4c
judasong <judasong@58.com>
2020-12-20 15:24:34 +0800

    复习算法，尝试新解法

--- src/inserttionsort/InsertionSort.java
+++ src/inserttionsort/InsertionSort.java
@@ -7,7 +7,7 @@ import utils.SortUtils;
 import java.lang.annotation.Annotation;
 
 /**
- *  插入排序的主要性质就是可以快速结束内层循环，特别适合基本有序的排序
+ * 插入排序的主要性质就是可以快速结束内层循环，特别适合基本有序的排序
  **/
 public class InsertionSort implements SortFlagInterface {
 
@@ -23,32 +23,21 @@ public class InsertionSort implements SortFlagInterface {
 //    }
 
     /**
-     * 优化后的插入排序，内层循环不在发现元素位置不对时就进行交换，而是在内层循环寻找array[i]合适的位置，这样省去2次交换
-     * 2020年02月28日11:58:06 运行结果不对呢？
-     * for (int i = 0; i < n; i++) {
-     *             int tep = array[i];
-     *             int j; //j表示arry[i]应该存放的位置
-     *             for (j = i; j > 0; j--){
-     *              //此处可以这么做的原因是，优化后的插入排序是为了找到array[i]应该在的位置，找到即可提前结束循环，这也是优化的点，减少循环次数
-     *                 //而冒泡是要挨个比较大小，把大的往后挪，故一次比较结果正确就结束不适合冒泡排序
-     *                 if (array[j-1] > tep){ //这一行也可以扔到for循环里，j>0后面
-     *                     array[j] = array[j-1];
-     *                 }
-     *             }
-     *             array[j] = tep;//
-     *         }
-     * 改成下面的代码可以了.上面不行的原因是j的值内层循环完肯定是0
-     *
+     * 优化后的插入排序，内层循环不在发现元素位置不对时就进行交换，而是在内层循环寻找array[i]合适的位置，这样省去2次交换 2020年02月28日11:58:06 运行结果不对呢？
+     * for (int i = 0; i < n; i++) { int tep = array[i]; int j; //j表示arry[i]应该存放的位置 for (j = i; j >
+     * 0; j--){ //此处可以这么做的原因是，优化后的插入排序是为了找到array[i]应该在的位置，找到即可提前结束循环，这也是优化的点，减少循环次数
+     * //而冒泡是要挨个比较大小，把大的往后挪，故一次比较结果正确就结束不适合冒泡排序 if (array[j-1] > tep){ //这一行也可以扔到for循环里，j>0后面
+     * array[j] = array[j-1]; } } array[j] = tep;// } 改成下面的代码可以了.上面不行的原因是j的值内层循环完肯定是0
      */
     public void sort(int[] array, int n) {
         for (int i = 1; i < n; i++) {
             int tep = array[i];//寻找arry[i]应该存放的合适位置
             int j; //j表示arry[i]应该存放的位置
-            for (j = i; j > 0 && array[j-1] > tep; j--){
+            for (j = i; j > 0 && array[j - 1] > tep; j--) {
                 //此处可以这么做的原因是，优化后的插入排序是为了找到array[i]应该在的位置，找到即可提前结束循环，这也是优化的点，减少循环次数
                 //而冒泡是要挨个比较大小，把大的往后挪，故一次比较结果正确就结束不适合冒泡排序
-                 //这一行也可以扔到for循环里，j>0后面
-                    array[j] = array[j-1];
+                //这一行也可以扔到for循环里，j>0后面
+                array[j] = array[j - 1];
             }
             array[j] = tep;
         }
@@ -87,11 +76,23 @@ public class InsertionSort implements SortFlagInterface {
         }
     }
 
+    public static void insertSort2(int[] arr) {
+        for (int i = 1, j; i < arr.length; i++) {
+            int current = arr[i];
+            for (j = i - 1; j >= 0 && arr[j] > current; j--) {
+                arr[j + 1] = arr[j];
+            }
+            //j+1的原因是上面内层for循环结束的时候j又--了一次
+            arr[j + 1] = current;
+        }
+    }
+
     public static void main(String[] args) throws InterruptedException {
-        int a[] = SortUtils.generateRandomIntArray(10, 0, 100);
+        int a[] = new int[]{2, 1};//SortUtils.generateRandomIntArray(10, 0, 100);
         SortUtils.printArray(a);
         System.err.println();
-        new InsertionSort().sort(a, 10);
+//        new InsertionSort().sort(a, 10);
+        InsertionSort.insertSort2(a);
         Thread.sleep(100);
         SortUtils.printArray(a);
 //        int a[] = SortUtils.generateRandomIntArray(50000, 0, 100000);
